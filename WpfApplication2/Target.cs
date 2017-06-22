eed8f6067ee1ebb2926022a21316a160940fbd74
avatar <avatar.opasne@gmail.com>
2017-06-22 14:38:33 +0400

    add target.cs

---
+++ WpfApplication2/Target.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prxSearcher
+{
+    [DataContract]
+    class Target
+    {
+        [DataMember]
+        public string mAdress { get; set; }
+        [DataMember]
+        public string mRegexContry { get; set; }
+
+        public override string ToString()
+        {
+            return html_parser.Replace(mAdress, "(?<=http(s)?://.*)/.*$", "");
+        }
+    }
+}
